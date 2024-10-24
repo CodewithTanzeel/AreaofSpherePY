e92d650fb8815fbc014ad4bd15c971281f15c752
TanzeelAhmad <Tanzeelofficial@outlook.com>
2024-10-24 22:48:33 +0500

    .

---
+++ index.py
@@ -0,0 +1,7 @@
+# Area of sphere 
+pi = 3.142 # assigning the value of pie
+r = float(input("Enter the value of radius: ")) # Taking input as floor because it will allow the user to put the floor values as well as the decimal 
+
+area = 4 * pi * r**2
+
+print ("The Area of Sphere is :", area)
